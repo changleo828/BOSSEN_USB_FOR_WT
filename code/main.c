09525be62da3473cc70b3a980f72e1751606acbc
Leo <changyijun828@126.com>
2012-11-29 17:31:44 +0800

    Create Write Close file

--- code/main.c
+++ code/main.c
@@ -3,9 +3,14 @@
 #include "HAL.h"
 #include "FILE_SYS.H"
 
+#include <string.h>
+#include <stdio.h>
+
 #ifndef NULL
 #define NULL (void *)0
 #endif
+
+UINT8		buf[64];
 
 void UartInit(void)
 {
@@ -20,7 +25,7 @@ void UartInit(void)
     UCA1MCTL = 0x06;
     UCA1IE = 0x01;
 }
-int putchar( int x )//注意不要改参数的类型和返回值的类型，否则printf调用是就有问题了。
+int putchar( int x )
 {
     while(!UCA1IFG__SPI_bit.UCTXIFG);
     UCA1TXBUF = x;
@@ -32,7 +37,6 @@ volatile unsigned char FlagRx;
 __interrupt void UART1RX (void)
 {
     RxBuffer = UCA1RXBUF;
-    //U0TXBUF = RxBuffer;
     FlagRx = 1;
 }
 void printk(char *str)
@@ -77,101 +81,67 @@ void	mStopIfError( UINT8 iErrCode ) {	/* 如果错误则停止运行并显示错误状态,正式应
 	if ( iErrCode == USB_INT_SUCCESS ) return;
 	printf( "Error status, %02X\n", (UINT16)iErrCode );
 }
+/* 例子:列举指定序号的目录下的所有文件 */
 
 int main( void )
 {
     int i;
     UINT8	res;
     UINT8	s;
-    //UINT8 name[] = "asdf.txt";
-    //unsigned char RD_Data;
+    UINT8	NameBuf[20] = {0};
     
     // Stop watchdog timer to prevent time out reset
     WDTCTL = WDTPW + WDTHOLD;
     P1DIR_bit.P1DIR0 = 1;
     P1OUT_bit.P1OUT0 = 1;
-    //AppUart_init();
-    UartInit();
 
+    UartInit();
+code_start:
     for(i = 0;i<30000;i++);
     printk("Code Start !\r\n");
+    printk("程序开始 !\r\n");
     
+    do{
+        for(i = 0;i<30000;i++);
+        res = mInitCH376Host();
+    }while(res != USB_INT_SUCCESS);
+    printk("USB_INT_SUCCESS !\r\n");
     
-#if 0
-    res = mInitCH376Host();
-    if(res == USB_INT_SUCCESS)
-        printk("USB_INT_SUCCESS !\r\n");
-    else
-        printk("USB_INT_ERR_USB_UNKNOWN !\r\n");
-    
+    do{
+        for(i = 0;i<30000;i++);
         res = CH376DiskConnect( ); /* 查询U盘是否连接，返回USB_INT_SUCCESS则说明当前已连接 */ 
-        if ( res==USB_INT_SUCCESS ) /* 已经连接 */ 
-            printk("  Connect SUCCESS\r\n");
-        else if ( res==ERR_DISK_DISCON ) /* 已经断开 */
-            printk("  ERR_DISK_DISCON\r\n");
-        else
-            printk("  ---------------\r\n");
-        
-        
-//初始化并检查U盘或者SD卡是否准备好，准备好后才能进行文件读写。示例： 
-        s=CH376DiskMount( ); /* 查询U盘或SD卡是否准备好，有些U盘可能需多次调用才能成功 */ 
-        if ( s!=USB_INT_SUCCESS ) 
-            printk("  -- U is not OK !\r\n");/* 还未准备好 */ 
-        else 
-            printk("  -- U is OK !\r\n");/* 准备好了，可以读写数据 */
-        
-        /* 在根目录或者当前目录下新建文件，如果文件已经存在则先删除再新建 */ 
-        CH376FileCreate( "/dBC.txt" );  
-        
-    CH376FileClose( TRUE );     /* 关闭当前已经打开的文件或者目录(文件夹) */ 
-    while(1){
+    }while(res!=USB_INT_SUCCESS);
+    printk("  Connect SUCCESS\r\n");
+    
+    do{
+        for(i = 0;i<30000;i++);
+        res = CH376DiskMount( ); /* 查询U盘或SD卡是否准备好，有些U盘可能需多次调用才能成功 */ 
+    }while(res!=USB_INT_SUCCESS);
+    printk("  U is OK !\r\n");
+
+    strcpy( NameBuf, "\\NEWFILE000.TXT" );  /* 目标文件名 */
+    printf( "Create\n" );
+    s = CH376FileCreatePath( NameBuf );  /* 新建多级目录下的文件,支持多级目录路径,输入缓冲区必须在RAM中 */
+    mStopIfError( s );
+    
+    printf( "Write\n" );
+    strcpy( buf, "找不到/C51/CH376HFT.C文件\xd\n" );
+    s = CH376ByteWrite( buf, strlen(buf), NULL );  /* 以字节为单位向当前位置写入数据块 */
+    mStopIfError( s );
+    
+    printf( "Close\n" );
+    s = CH376FileClose( TRUE );  /* 关闭文件,对于字节读写建议自动更新文件长度 */
+    mStopIfError( s );
+    
+    do{
         for(i = 0;i<30000;i++);
         P1OUT_bit.P1OUT0 = 1;
         for(i = 0;i<30000;i++);
-        P1OUT_bit.P1OUT0 = 0;
-    };
-#else
+        P1OUT_bit.P1OUT0 = 0;            
+        res = CH376DiskConnect( ); /* 查询U盘是否连接，返回USB_INT_SUCCESS则说明当前已连接 */ 
+        
+    }while(res!=ERR_DISK_DISCON);
+    printk("  ERR_DISK_DISCON\r\n");
+    goto code_start;
 
-	//mDelaymS( 10 );  /* 延时100毫秒 */
-    res = mInitCH376Host();
-    if(res == USB_INT_SUCCESS)
-        printk("USB_INT_SUCCESS !\r\n");
-    else
-        printk("USB_INT_ERR_USB_UNKNOWN !\r\n");
-	while ( 1 ) {
-		printk( "Insert USB disk\n" );
-		while ( CH376DiskConnect( ) != USB_INT_SUCCESS );  /* 等待U盘连接,SD卡模式下需要单片机自行检测SD卡插拔 */
-		mDelaymS( 250 );  /* 延时等待U盘进入正常工作状态 */
-		xWriteCH376Cmd( CMD_DISK_MOUNT );  /* 初始化U盘并测试是否就绪,实际是识别U盘的类型和文件系统,在所有读写操作之前必须进行此步骤 */
-		s = mWaitInterrupt( );  /* 等待中断并获取状态 */
-		if ( s != USB_INT_SUCCESS ) {  /* 有的U盘一次初始化不成功,再试一次 */
-			mDelaymS( 100 );  /* 出错重试 */
-			xWriteCH376Cmd( CMD_DISK_MOUNT );  /* 初始化磁盘并测试磁盘是否就绪 */
-			s = mWaitInterrupt( );  /* 等待中断并获取状态 */
-		}
-		mStopIfError( s );
-		xWriteCH376Cmd( CMD_SET_FILE_NAME );  /* 设置将要操作的文件的文件名,通配符支持所有文件和子目录 */
-		xWriteCH376Data( '/' );
-		xWriteCH376Data( '*' );
-		xWriteCH376Data( 0 );
-		xWriteCH376Cmd( CMD_FILE_OPEN );  /* 枚举文件和目录 */
-		while ( 1 ) {
-			s = mWaitInterrupt( );
-			if ( s == USB_INT_DISK_READ ) {  /* 请求数据读出 */
-				xWriteCH376Cmd( CMD01_RD_USB_DATA0 );
-				printk( "Name: ");
-				xReadCH376Data( );  /* 长度总是sizeof(FAT_DIR_INFO) */
-				for ( s = 0; s != 8+3; s ++ ) printf( "%c", xReadCH376Data( ) );
-				printf( "         (%c)\n", xReadCH376Data( )&ATTR_DIRECTORY?'#':'*' );  /* 目录显示#后缀,文件显示*后缀 */
-				for ( s = 11; s != sizeof( FAT_DIR_INFO ); s ++ ) xReadCH376Data( );  /* 取出后续数据 */
-				xWriteCH376Cmd( CMD0H_FILE_ENUM_GO );  /* 继续枚举文件和目录 */
-			}
-			else if ( s == ERR_MISS_FILE ) break;  /* 没有找到更多的匹配文件,正常结束 */
-			else mStopIfError( s );  /* 操作出错 */
-		}
-		while ( mWaitInterrupt( ) != USB_INT_DISCONNECT );  /* 等待U盘拔出 */
-		mDelaymS( 250 );
-	}
-#endif
-    //return 0;
 }
